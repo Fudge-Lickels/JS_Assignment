67a2db324a9506c46fcab31e413b7b0140d62823
Fudge-Lickels <107988982+Fudge-Lickels@users.noreply.github.com>
2024-05-24 19:07:08 +0200

    Initial commit

---
+++ public_html/MathShapes.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Lenght of daiagonal square
+let vertiLenght = 9;
+let horiLenght = Math.sqrt(2* Math.pow(vertiLenght, 2));
+console.log("Diagonal of a square is:", horiLenght.toFixed(2));
